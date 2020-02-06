0452080d04d7023c49292afd9e0361d4622249f3
willa <willa@pop-os.localdomain>
2020-02-05 23:47:01 -0300

    chore: adciona tabela courier

---
+++ src/app/models/Courier.js
@@ -0,0 +1,21 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Courier extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+      },
+      {
+        sequelize,
+      }
+    );
+
+    // todo association avatar_id
+
+    return this;
+  }
+}
+
+export default Courier;
