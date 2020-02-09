a52c33d84ba8006595984868339c754223c58c31
willa <willa@pop-os.localdomain>
2020-02-09 00:03:34 -0300

    chore: adciona validacao na criacao dos couriers

--- src/app/controllers/CourierController.js
+++ src/app/controllers/CourierController.js
@@ -1,7 +1,19 @@
+import * as Yup from 'yup';
 import Courier from '../models/Courier';
 
 class CourierController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
     const { id, name, email } = await Courier.create(req.body);
 
     return res.json({
